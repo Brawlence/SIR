210b9a4399c4581c5ba600606a9bc1a595daeb31
Brawlence <42910943+Brawlence@users.noreply.github.com>
2018-09-03 17:48:02 +0300

    Deviantart and pixiv basic rename

--- Extention/backgroundListener.js
+++ Extention/backgroundListener.js
@@ -10,23 +10,42 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 	// separate extension from the filename
 	var ext = ChromeFilename.substr(ChromeFilename.lastIndexOf('.') + 1);
 	var filename = ChromeFilename.substring(0, ChromeFilename.lastIndexOf('.'));
+	
+	// extract the domain from hostname - indexOf is freakingly fast, see https://jsperf.com/substring-test
+	if (hostname.indexOf('deviantart') > -1) {
+		// author is a string starting after _by_ and ending with the last '-'
+		var author = filename.substring(filename.lastIndexOf('_by_')+4,filename.lastIndexOf('-'));
+		// reformat filename to this template, moving the author info
+		filename = '[' + author + '@DA] ' + filename.substring(0, filename.lastIndexOf('_by_'));
+	};
 
-	switch (hostname) {
-		case 'twitter':
-			filename = '[TW~___] ' + filename;		
-			break;
-		case 'deviantart.net':
-			filename = '[DA~___] ' + filename;
-			break;
-		case 'pixiv':
-			filename = '[PX~___] ' + filename;
-			break;
-		case 'tumblr':
-			filename = '[TU~___] ' + filename;
-			break;
-		default:
-			break;
-	}
+	if (hostname.indexOf('tumblr') > -1) {
+		filename = '[' + author + '@TU@] ' + filename;
+	};
+	
+	if (hostname.indexOf('twimg') > -1) {
+		filename = '[' + author + '@TW] ' + filename;
+	};
+	
+	if (hostname.indexOf('pximg') > -1) {
+		//since pixiv does not give info about name and author, try to extract it from the title of the page
+		chrome.tabs.getSelected(null, function(tab) {
+		alert(tab.title);       
+		});
+		var doctitle = tab.title;
+		var author = doctitle.substring(doctitle.lastIndexOf('「'),doctitle.lastIndexOf('」'));
+		var name = doctitle.substring(doctitle.indexOf('「'),doctitle.indexOf('」'));
+		alert(tab.title + ' ' +  doctitle + ' ' + author + ' ' + name);
+		
+		// if user wants to save a rescaled thumbnail, add a tag
+		if (filename.indexOf('master') > -1) {
+			filename = 'THUMBNAIL!' + filename.substring(0, filename.lastIndexOf('_master'));
+		};
+		
+		var number = filename.substring(0,filename.lastIndexOf('_'));
+		filename = '[' + author + '@PX] pixiv_' + number + ' ' + name;
+	};
+
 
 	// make sure the name is not left blank
 	if (filename == "") {
@@ -35,9 +54,9 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 
 	// add back the extension to the file name
 	filename = filename + "." + ext;
-
+	
 	// make sure the modified filename doesn't contain any illegal characters
-	filename = filename.replace(/[\#\?].*$/,'_');
+	filename = filename.replace(/[\#\?].*$/,'');
 
 	// suggest the new filename to Chrome
 	suggest({ filename: filename, conflictAction: "uniquify" });
