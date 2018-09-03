9dc9f7f5632771358194e476dc905c10c4a437f3
Brawlence <42910943+Brawlence@users.noreply.github.com>
2018-09-03 15:43:25 +0300

    Primitive renaming functionality
    Need to improve hostname detection and selector

---
+++ Extention/contextMenu.js
@@ -0,0 +1,47 @@
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+
+	var downloadInfo = {};
+		
+	// get where that image is hosted
+	var a = document.createElement('a');
+	a.href = item.url;
+	var hostname = a.hostname;
+
+	// get filename determined by Chrome
+	var ChromeFilename = item.filename;
+	
+	// separate extension from the filename
+	var ext = ChromeFilename.substr(ChromeFilename.lastIndexOf('.') + 1);
+	var filename = ChromeFilename.substring(0, ChromeFilename.lastIndexOf('.'));
+
+	switch (hostname) {
+		case 'twitter':
+			filename = '[TW~___] ' + filename;		
+			break;
+		case 'deviantart.net':
+			filename = '[DA~___] ' + filename;
+			break;
+		case 'pixiv':
+			filename = '[PX~___] ' + filename;
+			break;
+		case 'tumblr':
+			filename = '[TU~___] ' + filename;
+			break;
+		default:
+			break;
+	}
+
+	// make sure the name is not left blank
+	if (filename == "") {
+		filename = "tagme";
+	}
+
+	// add back the extension to the file name
+	filename = filename + "." + ext;
+
+	// make sure the modified filename doesn't contain any illegal characters
+	filename = filename.replace(/[\#\?].*$/,'_');
+
+	// suggest the new filename to Chrome
+	suggest({ filename: filename, conflictAction: "uniquify" });
+});
