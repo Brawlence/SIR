54f44726717e3a7654a72b88cdb2eadba4c4afa3
Brawlence <42910943+Brawlence@users.noreply.github.com>
2018-09-05 02:37:07 +0300

    FINALLY
    As the active tab changes, the listener reads its' title. Which is then used through the global variable during the save.

--- Extention/backgroundListener.js
+++ Extention/backgroundListener.js
@@ -1,77 +1,94 @@
+//global variable with the active tab title, updated every time user tries to change the active tab
+var tNfo = 'empty_name';
 
-
+chrome.tabs.onActivated.addListener( function(info) {
+	chrome.tabs.query({active: true, currentWindow: true },  function sasuga(tabs) {
+		localStorage["current_title"] = tabs[0].title;
+		tNfo=tabs[0].title;
+	});
+});
 
 chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
-	// get where that image is hosted
-	var a = document.createElement('a');
-	a.href = item.url;
-	var hostname = a.hostname;
+	
+	//if we still don't know the active tab title
+	if (tNfo == 'empty_name') {
+		//break the download
+		chrome.downloads.cancel(item.id);
 		
-	// get filename determined by Chrome
-	var ChromeFilename = item.filename;
-	
-	// separate extension from the filename
-	var ext = ChromeFilename.substr(ChromeFilename.lastIndexOf('.') + 1);
-	var filename = ChromeFilename.substring(0, ChromeFilename.lastIndexOf('.'));
-	
-	// extract the domain from hostname - indexOf is freakingly fast, see https://jsperf.com/substring-test
-	// DEVIANTART
-	if (hostname.indexOf('deviantart') > -1) {
-		// author is a string starting after _by_ and ending with the last '-'
-		var author = filename.substring(filename.lastIndexOf('_by_')+4,filename.lastIndexOf('-'));
-		// reformat filename to this template, moving the author info
-		filename = '[' + author + '@DA] ' + filename.substring(0, filename.lastIndexOf('_by_'));
+	} else {
+		
+		//MEANINGFUL NAMING
+		
+			// get where that image is hosted
+			var a = document.createElement('a');
+			a.href = item.url;
+			var hostname = a.hostname;
+				
+			// get filename determined by Chrome
+			var ChromeFilename = item.filename;
+			
+			// separate extension from the filename
+			var ext = ChromeFilename.substr(ChromeFilename.lastIndexOf('.') + 1);
+			var filename = ChromeFilename.substring(0, ChromeFilename.lastIndexOf('.'));
+			
+			// extract the domain from hostname - indexOf is freakingly fast, see https://jsperf.com/substring-test
+			// DEVIANTART
+			if (hostname.indexOf('deviantart') > -1) {
+				// author is a string starting after _by_ and ending with the last '-'
+				var author = filename.substring(filename.lastIndexOf('_by_')+4,filename.lastIndexOf('-'));
+				// reformat filename to this template, moving the author info
+				filename = '[' + author + '@DA] ' + filename.substring(0, filename.lastIndexOf('_by_'));
+			};
+			
+			//TUMBLR
+			//Add parser for <figcaption>AUTHOR</figcaption>
+			if (hostname.indexOf('tumblr') > -1) {
+				filename = '[' + author + '@TU@] ' + filename;
+			};
+			
+			//TWITTER
+			if (hostname.indexOf('twimg') > -1) {
+				filename = '[' + author + '@TW] ' + filename;
+				//cleaning ext - twitter links are nasty
+				if (ext.indexOf('large') > -1) { ext = ext.substring(0, ext.indexOf('-large')); };
+			};
+			
+			//PIXIV
+			//The illustration name is always in <h1>NAME</h1>
+			if (hostname.indexOf('pximg') > -1) {
+				//since pixiv does not give info about name and author, try to extract it from the title of the page
+
+				var author = tNfo.substring(tNfo.lastIndexOf('\u300C')+1,tNfo.lastIndexOf('\u300D'));
+				var name = tNfo.substring(tNfo.indexOf('\u300C')+1,tNfo.indexOf('\u300D'));
+				//alert('The resulting author-picture is ' + author + ' - ' + name);
+				
+				// if user wants to save a rescaled thumbnail, add a tag
+				if (filename.indexOf('master') > -1) {
+					filename = 'THUMBNAIL!' + filename.substring(0, filename.lastIndexOf('_master'));
+				};
+				
+				var number = filename.substring(0,filename.lastIndexOf('_'));
+				filename = '[' + author + '@PX] pixiv_' + number + ' ' + name;
+
+				};
+
+
+			// make sure the name is not left blank
+			if (filename == "") {
+				filename = "tagme";
+			}
+
+			// add back the extension to the file name
+			filename = filename + "." + ext;
+			
+			// make sure the modified filename doesn't contain any illegal characters
+			filename = filename.replace(/[\#\?].*$/,'');
+			
+		//end of MEANINGFUL NAMING
+		
+		// suggest the new filename to Chrome IF getting the tab name was successeffull
+		suggest({ filename: filename, conflictAction: "uniquify" });
+
 	};
-	
-	//TUMBLR
-	//Add parser for <figcaption>AUTHOR</figcaption>
-	if (hostname.indexOf('tumblr') > -1) {
-		filename = '[' + author + '@TU@] ' + filename;
-	};
-	
-	//TWITTER
-	if (hostname.indexOf('twimg') > -1) {
-		filename = '[' + author + '@TW] ' + filename;
-		//cleaning ext - twitter links are nasty
-		if (ext.indexOf('large') > -1) { ext = ext.substring(0, ext.indexOf('-large')); };
-	};
-	
-	//PIXIV
-	//The illustration name is always in <h1>NAME</h1>
-	if (hostname.indexOf('pximg') > -1) {
-		//since pixiv does not give info about name and author, try to extract it from the title of the page
-		chrome.tabs.query({ active: true, currentWindow: true },  function sasuga(tabs) {
-			title=tabs[0].title;
-			alert(title);
-		});
-		
-		alert(title);
-		var author = title.substring(title.lastIndexOf('\u300C')+1,title.lastIndexOf('\u300D'));
-		var name = title.substring(title.indexOf('\u300C')+1,title.indexOf('\u300D'));
-		alert(author + ' - ' + name);
-		
-		// if user wants to save a rescaled thumbnail, add a tag
-		if (filename.indexOf('master') > -1) {
-			filename = 'THUMBNAIL!' + filename.substring(0, filename.lastIndexOf('_master'));
-		};
-		
-		var number = filename.substring(0,filename.lastIndexOf('_'));
-		filename = '[' + author + '@PX] pixiv_' + number + ' ' + name;
-
-		};
-
-
-	// make sure the name is not left blank
-	if (filename == "") {
-		filename = "tagme";
-	}
-
-	// add back the extension to the file name
-	filename = filename + "." + ext;
-	
-	// make sure the modified filename doesn't contain any illegal characters
-	filename = filename.replace(/[\#\?].*$/,'');
-
-	// suggest the new filename to Chrome
-	suggest({ filename: filename, conflictAction: "uniquify" });
+	tNfo = 'empty_name';
 });
