1201f6d6a0c5f074b003fa5069b838292ef84b41
Brawlence <42910943+Brawlence@users.noreply.github.com>
2019-04-24 08:00:42 +0300

    Cleaning code

--- Extention/DownloadsInject.js
+++ Extention/DownloadsInject.js
@@ -8,17 +8,10 @@ function nicelyTagIt(imageHost, requesterPage, chromeFilename) { // gets filenam
 
 	var filename = chromeFilename.substring(0, chromeFilename.lastIndexOf('.'));
 	var ext = chromeFilename.substr(chromeFilename.lastIndexOf('.') + 1); // separate extension from the filename
-	
 
 	// indexOf is freakingly fast, see https://jsperf.com/substring-test
 	// ! DEVIANTART
 	if ( (imageHost.indexOf('deviantart') > -1) || (requesterPage.indexOf('deviantart') > -1) ) {
-	
-		if (filename.indexOf('_by_') > -1) { // old method based on file name from the link
-			var author = filename.substring(filename.lastIndexOf('_by_')+4,filename.lastIndexOf('-')); // author is a string starting after _by_ and ending with the last '-'
-			var name = filename.substring(0, filename.lastIndexOf('_by_'));
-		}
-		
 		var author = activeTabTitle.substring(activeTabTitle.lastIndexOf(' by ')+4,activeTabTitle.lastIndexOf(' on Deviant'));
 		var name = activeTabTitle.substring(0, activeTabTitle.lastIndexOf(' by '));
 
@@ -67,10 +60,7 @@ function nicelyTagIt(imageHost, requesterPage, chromeFilename) { // gets filenam
 	};
 	
 	// ! PIXIV
-	if ((imageHost.indexOf('pximg') > -1)||(requesterPage.indexOf('pixiv') > -1)) {
-		// old Pixiv naming standart
-		//var author = activeTabTitle.substring(activeTabTitle.lastIndexOf('\u300C')+1,activeTabTitle.lastIndexOf('\u300D')); // '「' and '」' as last indexes
-		//var name = activeTabTitle.substring(activeTabTitle.indexOf('\u300C')+1,activeTabTitle.indexOf('\u300D'));
+	if ( (imageHost.indexOf('pximg') > -1) || (requesterPage.indexOf('pixiv') > -1) ) {
 		var splotted = activeTabTitle.split('\"');
 		var name = splotted[1];
 		var author = splotted[3];
@@ -78,17 +68,17 @@ function nicelyTagIt(imageHost, requesterPage, chromeFilename) { // gets filenam
 		var PXnumber = filename.substring(0,filename.lastIndexOf('_'));
 		var PXpage = filename.substring(filename.lastIndexOf('_p')+2, filename.lastIndexOf('_p')+4);
 
-		if (filename.indexOf('master') > -1) { // if user wants to save a rescaled thumbnail, add a tag
-			filename = 'THUMBNAIL!' + filename.substring(0, filename.lastIndexOf('_master')); 
+		if (filename.indexOf('master') > -1) { 
+			filename = 'THUMBNAIL!' + filename.substring(0, filename.lastIndexOf('_master')); // if user wants to save a rescaled thumbnail, add a tag
 		};
 			
 		filename = '[' + author + '@PX] pixiv_' + PXnumber + '_' + PXpage + ' ' + name;
 
 		if (localStorage["origin"] === "PX") {
 			var arrayOfTags = JSON.parse(localStorage["tags"]);
-				for (i = 0; i < arrayOfTags.length; i++) {
-					filename = filename + " " + arrayOfTags[i];
-				};
+			for (i = 0; i < arrayOfTags.length; i++) {
+				filename = filename + " " + arrayOfTags[i];
+			};
 		}
 	};
 
@@ -115,9 +105,9 @@ function nicelyTagIt(imageHost, requesterPage, chromeFilename) { // gets filenam
 
 		if (localStorage["origin"] === "HF") {
 			var arrayOfTags = JSON.parse(localStorage["tags"]);
-				for (i = 0; i < arrayOfTags.length; i++) {
-					filename = filename + " " + arrayOfTags[i];
-				};
+			for (i = 0; i < arrayOfTags.length; i++) {
+				filename = filename + " " + arrayOfTags[i];
+			};
 		}
 	};
 
@@ -127,18 +117,16 @@ function nicelyTagIt(imageHost, requesterPage, chromeFilename) { // gets filenam
 		filename = "tagme"; 
 	}
 
-	if (ext == "") {
+	if (ext == "") { // also make sure that extention did not magically disappear
 		ext == "maybe.jpeg";
 	}
 	
 	filename = filename + "." + ext; // add back the extension to the file name
 	
 	//console.log("Renaming result: " + filename);
-	//alert(filename);
+
 	return filename;
 };
-
-
 
 function requestTagsAndWriteEm(TabIdToSendTo) {
 	chrome.tabs.sendMessage(TabIdToSendTo, {order: "giffTags"},
@@ -149,7 +137,6 @@ function requestTagsAndWriteEm(TabIdToSendTo) {
 				if (typeof response !== 'undefined') {
 					localStorage["tags"] = JSON.stringify(response.tags);
 					localStorage["origin"] = response.origin;
-					//console.log(response.tags);
 				}
 			}
 		}
@@ -161,8 +148,10 @@ chrome.tabs.onActivated.addListener(
 		chrome.tabs.query({active: true, currentWindow: true, status: "complete"}, // send into query (if active tab and current window were true)
 			function setStorage(tabs) {
 				if (tabs.length > 0) {
-					//console.log("onActivated returned those values: " + tabs[0].title + " " + tabs[0].url);
 					localStorage["active_tab_title"] = tabs[0].title;			// needs the "tabs" permission
+					
+					// somewhy the following wrecks havoc on everything
+					// TODO: I need to think how to actually switch current line of tags when the active tab is changed 
 					//requestTagsAndWriteEm(tabs[0].id);
 				}
 			}
@@ -180,7 +169,6 @@ chrome.tabs.onUpdated.addListener(
 					if (typeof tabs !== 'undefined') {
 						if (tabs.length > 0) {
 							if (tabs[0].tabId === culprit.tabId) { // Definitely the ACTIVE one was updated
-								//console.log("onUpdated! Culprit: " + culprit.url + " " + culprit.title + "\n Active tab:" + tabs[0].title + " " + tabs[0].url);
 								localStorage["active_tab_title"] = tabs[0].title; 	// needs the "tabs" permission
 								requestTagsAndWriteEm(tabs[0].id);
 							}
@@ -190,32 +178,28 @@ chrome.tabs.onUpdated.addListener(
 			);
 		}
 	}
-); 
-
+);
 
 chrome.downloads.onDeterminingFilename.addListener(
-	function swapTheFilename(item, suggest) { // sent into DeterminingFilename
-	// Has access only to chrome's background page
+	function swapTheFilename(item, suggest) {
+	
+		// get where that image is hosted on by 
+		var tempContainer = document.createElement('a'); 									// creating an link-type (a) object 
+		tempContainer.href = item.url;														// ! linking to the item we are about to save
+		//tempContainer.innerHTML = "Heh, not bad, kid. You made me use my HTML power!"; 	// with inner HTML structure
+		//document.body.appendChild(tempContainer);											// on chrome's generated background page
 
-			// get where that image is hosted on by 
-			var tempContainer = document.createElement('a'); 									// creating an link-type (a) object 
-			tempContainer.href = item.url;														// ! linking to the item we are about to save
-			//tempContainer.innerHTML = "Heh, not bad, kid. You made me use my HTML power!"; 	// with inner HTML structure
-			//document.body.appendChild(tempContainer);											// on chrome's generated background page
-
-			// at we can see, item.hostname is undefined
-			//console.log("Item URL: " + item.url + ", Item hostname: " + item.hostname);					
-			// but if we do this, suddenly url is undefined but hostname works
-			//console.log("temp object URL: " + tempContainer.url + ", temp object hostname: " + tempContainer.hostname);
-			
-			var imageHost = tempContainer.hostname;
-			
-			localStorage["initiator_url"] = item.referrer; // TODO: remove or comment this - it's not necessary, just convinient to debug
-
-			var resultingFilename = nicelyTagIt(imageHost, item.referrer, item.filename);
-			
-			//end of MEANINGFUL NAMING
-			// suggest the new filename to Chrome IF getting the tab name was successeffull
-			suggest({ filename: resultingFilename, conflictAction: "uniquify" });
+		// at we can see, item.hostname is undefined
+		//console.log("Item URL: " + item.url + ", Item hostname: " + item.hostname);					
+		// but if we do this, suddenly url is undefined but hostname works
+		//console.log("temp object URL: " + tempContainer.url + ", temp object hostname: " + tempContainer.hostname);
+		
+		var imageHost = tempContainer.hostname;
+		
+		var resultingFilename = nicelyTagIt(imageHost, item.referrer, item.filename);
+		
+		// end of MEANINGFUL NAMING
+		// suggest the new filename to Chrome IF getting the tab name was successeffull
+		suggest({ filename: resultingFilename, conflictAction: "uniquify" });
 	}
 );
