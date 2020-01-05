262862da552255d7aaab3f1054c71f39559b35a4
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-01-05 22:06:40 +0300

    DEVIANART fixes, code readableness
    Unified tag fetching for both Eclipse and Old Deviantart design, made the code more readable — now buttons have their onclick() javascrip on newlines, improved robustness of adressing where to append Elder-Magic-Field and from where to remove it.

--- Extention/DF/tagsParser_content_script.js
+++ Extention/DF/tagsParser_content_script.js
@@ -8,19 +8,33 @@ function getImageTags() {
 	return resultingTags;
 };
 
+//this one is different - instead of the popup field like everyone else this uses the existing tags text field on the page
 function createElderMagicField() {
 	if (document.getElementById('sirArea') == null) {
 		document.getElementById('edit_form').style = "display: ";
 		const buttonsParagraph = document.createElement('p');
 		buttonsParagraph.id = "sirArea";
-		buttonsParagraph.innerHTML = "<button id=\"c-and-h\" onclick=\"javascript:document.getElementById('tags').select();document.execCommand('copy');document.getElementById('sirArea').parentElement.style='';document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));document.getElementById('edit_form').style = 'display:none';\">Copy & Hide</button>";
+		buttonsParagraph.innerHTML =
+			"<button id=\"c-and-h\" style=\"float:right\" onclick=\"javascript:\
+				document.getElementById('tags').select();\
+				document.execCommand('copy');\
+				document.getElementById('sirArea').parentElement.style='';\
+				document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));\
+				document.getElementById('edit_form').style = 'display:none';\">\
+			Copy & Hide</button>\
+			<button onclick=\"javascript:\
+				document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));\">\
+			Cancel</button>";
 
 		document.getElementById('tags').parentElement.appendChild(buttonsParagraph);
-		document.getElementById('tags').parentElement.style = "height: 150px; width: 360px;  border-width: 3px; border-style: solid; padding: 5px; background-color: lightgray;";
+		document.getElementById('tags').parentElement.style =
+			"height: 150px; width: 360px;  border-width: 3px; border-style: solid; padding: 5px; background-color: lightgray;";
+
 		document.getElementById('c-and-h').focus();
+
 	} else {
-		document.getElementById('tags').parentElement.removeChild(document.getElementById('sirArea'));
 		document.getElementById('tags').parentElement.style = "";
+		document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));
 	}
 };
 
--- Extention/PX/tagsParser_content_script.js
+++ Extention/PX/tagsParser_content_script.js
@@ -6,7 +6,7 @@ function getImageTags() {
 	var pictureName = document.querySelectorAll('figcaption div div h1');
 	var tempArray = document.querySelectorAll('figcaption div footer ul li a');
 
-	resultingTags.push(authorName[0].innerText+"@PX");
+	resultingTags.push(authorName[0].innerText + "@PX");
 	resultingTags.push(pictureName[0].innerText);
 	for (i = 0; i < tempArray.length; i++) {
 		resultingTags.push(tempArray[i].innerText);
@@ -21,26 +21,34 @@ function createElderMagicField() {
 		for (i = 0; i < arrayOfTags.length; i++) {
 			tagsString += arrayOfTags[i].replace(/ /g, '_') + " ";
 		};
-	
+
 		const sirDivArea = document.createElement('div');
-			sirDivArea.id = "sirArea";
-			sirDivArea.style = "top: 20px; left: 20px; position: fixed; z-index: 255;  border-width: 3px; border-style: solid; padding: 5px; background-color: lightgray;"
+		sirDivArea.id = "sirArea";
+		sirDivArea.style = "top: 20px; left: 20px; position: fixed; z-index: 255;  border-width: 3px; border-style: solid; padding: 5px; background-color: lightgray;"
 		const elderMagicField = document.createElement('textarea');
-			elderMagicField.id = "elderMagicField";
-			elderMagicField.style = "height: 150px; width: 360px;";
-			elderMagicField.value = tagsString;
+		elderMagicField.id = "elderMagicField";
+		elderMagicField.style = "height: 150px; width: 360px;";
+		elderMagicField.value = tagsString;
 		//somewhy I can't add onclick events to buttons with this method so I'll have to use innerHTML instead
 		const buttonsParagraph = document.createElement('p');
-			buttonsParagraph.innerHTML = "<button id=\"c-and-h\" style=\"float:right\" onclick=\"javascript:document.getElementById('elderMagicField').select();document.execCommand('copy');document.getElementById('root').removeChild(document.getElementById('sirArea'));\">Copy & Hide</button><button onclick=\"javascript:document.getElementById('root').removeChild(document.getElementById('sirArea'));\">Cancel</button>";
-	
-		document.getElementById('root').appendChild(sirDivArea);
+		buttonsParagraph.innerHTML =
+			"<button id=\"c-and-h\" style=\"float:right\" onclick=\"javascript:\
+				document.getElementById('elderMagicField').select();\
+				document.execCommand('copy');\
+				document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));\">\
+			Copy & Hide</button>\
+			<button onclick=\"javascript:\
+				document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));\">\
+			Cancel</button>";
+
+		document.body.appendChild(sirDivArea);
 		document.getElementById('sirArea').appendChild(elderMagicField);
 		document.getElementById('sirArea').appendChild(buttonsParagraph);
-	
+
 		document.getElementById('c-and-h').focus();
-	
+
 	} else {
-		document.getElementById('root').removeChild(document.getElementById('sirArea'));
+		document.getElementById('sirArea').parentElement.removeChild(document.getElementById('sirArea'));
 	}
 };
 
