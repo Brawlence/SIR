2b9bb0f8ff5275e116471f47d0f3687a2de33e12
Brawlence <42910943+Brawlence@users.noreply.github.com>
2020-01-20 21:38:28 +0300

    Hotfix for PIXIV authorName@PX handling
    Now ____@PX should be correctly displayed.

--- Extension/PX/tagsParser.js
+++ Extension/PX/tagsParser.js
@@ -3,11 +3,11 @@ var windowDisplacement = 0;
 
 function getImageTags() {
 	var resultingTags = new Array;
-	var authorName = document.querySelector('aside section h2 div div a div').innerText;
+	var authorHandle = document.querySelector('aside section h2 div div a div').innerText;
 	var pictureName = document.querySelector('figcaption div div h1').innerText;
 	var tempArray = document.querySelectorAll('figcaption div footer ul li a');
 
-	resultingTags.push(authorName.replace(/[ ]/g, '-'));
+	resultingTags.push(authorHandle.replace(/[ ]/g, '-') + "@" + tagsOrigin);
 	resultingTags.push(pictureName.replace(/[ ]/g, '-'));
 
 	for (var i = 0; i < tempArray.length; i++) {
